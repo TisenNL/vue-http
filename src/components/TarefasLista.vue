11d809f95971b0c85f80b37ae547b36b7a7bc810
Tiago Lino <tiagobrasil18@gmail.com>
2022-12-15 07:04:55 -0300

    study session 15-12-2022

--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -19,16 +19,20 @@
 
         <ul class="list-group" v-if="tarefas.length > 0">
             <TarefasListaIten
-                v-for="tarefa in tarefas"
-                :key="tarefa.id"
-                :tarefa="tarefa" />
+              v-for="tarefa in tarefas"
+              :key="tarefa.id"
+              :tarefa="tarefa"
+              @editar="selecionarTarefaParaEdicao"
+            />
         </ul>
 
         <p v-else>Nenhuma tarefa criada.</p>
 
         <TarefaSalvar
           v-if="exibirFormulario"
+          :tarefa="tarefaSelecionada"
           @criar="criarTarefa"
+          @editar="editarTarefa"
         />
 
     </div>
@@ -51,7 +55,8 @@ export default {
     data() {
         return {
             tarefas: [],
-            exibirFormulario: false
+            exibirFormulario: false,
+            tarefaSelecionada: undefined
         }
     },
     created() {
@@ -69,6 +74,13 @@ export default {
                     this.tarefas.push(response.data)
                     this.exibirFormulario = false
                 })
+        },
+        editarTarefa(tarefa) {
+            console.log('Editar: ', tarefa)
+        },
+        selecionarTarefaParaEdicao(tarefa) {
+             this.tarefaSelecionada = tarefa
+             this.exibirFormulario = true
         }
     },
 }
